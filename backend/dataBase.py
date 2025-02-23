9381dbde8397a83f8301d39b5362a2a52a68aa6e
Jokinth <115614292+Jokinth@users.noreply.github.com>
2025-02-23 21:36:41 +0530

    Update dataBase.py

--- backend/dataBase.py
+++ backend/dataBase.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 
 # Replace with your actual MongoDB URI from Railway
-MONGODB_URI = "mongodb+srv://<username>:<password>@cluster0.mongodb.net/test?retryWrites=true&w=majority"
+MONGODB_URI = "mongodb://mongo:FEbHVqzLCTpeDSxLvKWAYXzZqiMMiCBw@maglev.proxy.rlwy.net:52256"
 
 client = MongoClient(MONGODB_URI)
 db = client.get_database()  # You can specify the name of the database here
