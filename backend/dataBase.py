957c0ec92ade7e13259c4321d5147c0b16871c47
Jokinth <115614292+Jokinth@users.noreply.github.com>
2025-02-25 13:53:04 +0530

    Update dataBase.py

--- backend/dataBase.py
+++ backend/dataBase.py
@@ -1,11 +1,11 @@
 from pymongo import MongoClient
 
 # MongoDB URI with database name in the connection string
-MONGODB_URI = 'mongodb://mongo:FEbHVqzLCTpeDSxLvKWAYXzZqiMMiCBw@maglev.proxy.rlwy.net:52256/my_database'
+MONGODB_URI = 'mongodb://mongo:FEbHVqzLCTpeDSxLvKWAYXzZqiMMiCBw@maglev.proxy.rlwy.net:52256'
 
 client = MongoClient(MONGODB_URI)
 db = client['my_database']  # Use the database name here
 
-# Now you can access collections
-collection = db['your_collection_name']
+# Define collections
+collection = db['symptoms']
 users_collection = db['users']
